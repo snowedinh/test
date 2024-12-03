eadd06f55e5275dc5fba216a6f916ea89e035cc8
snowedinh <jsr15308300998@gmail.com>
2024-12-03 14:58:02 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import os
+
+
 
 st.set_page_config(layout="wide")
 
@@ -15,26 +18,22 @@ logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("空谱融合的地块级耕地种植状况遥感监测")
 
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/opengeos/streamlit-map-template).
+    融合高空间分辨率遥感影像的空间信息和时间序列遥感数据的谱信息，构建空谱融
+合的遥感模型，实现江汉平原地块级的耕地种植状况监测
     """
 )
 
-st.header("Instructions")
+st.header("研究区域的介绍")
 
 markdown = """
-1. For the [GitHub repository](https://github.com/opengeos/streamlit-map-template) or [use it as a template](https://github.com/opengeos/streamlit-map-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
-
+以江汉平原地区一块小区域（如图所示）进行实证研究
 """
-
 st.markdown(markdown)
 
-m = leafmap.Map(minimap_control=True)
+m = leafmap.Map(center=[30.5469,112.8212],zoom=10,minimap_control=True)
 m.add_basemap("OpenTopoMap")
 m.to_streamlit(height=500)
